d1f3d4bee3a91ee728c26152615c1f369c7efa25
Sokmontrey <sythatsokmontrey@gmail.com>
2023-01-02 21:05:54 -0500

    rewrite test with a different design. NOTE: not complete yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,49 @@
 #include <iostream>
 #include <vector>
 
-//PROBLEM: vector of _nodes are not a vector of references to nodes, but rather a vector of the copy of the nodes
 template <typename T>
 class Node {
-	private:
-		T _value;
-		T _dValue;
+private:
+	T _value;
+	T _d_value;
 
-		std::vector<Node<T>> _nodes;
-
-		bool _isOperatorNode = false;
-
-	public:
-		Node() = default;
-		Node(T value): _value(value) {}
-
-		template <typename... Args>
-		Node(Args... nodes){
-			for(const auto& node: {nodes...}){
-				this->_nodes.push_back(node);
-			}
-			this->_isOperatorNode = true;
-		}
-
-		T compute(){
-			if(this->_isOperatorNode){
-				this->_value = 1;
-				for(int i=0; i<this->_nodes.size(); i++){
-					this->_value *= this->_nodes[i].compute();
-				}
-			}
-			this->_dValue = 0;
-			return this->_value;
-		}
-
-		void derivative(T chain_value){
-			if(this->_isOperatorNode){
-				for(int i=0; i<this->_nodes.size(); i++){
-					this->_nodes[i].derivative(chain_value * this->getValue() / this->_nodes[i].getValue());
-				}
-			}else{
-				this->_dValue += chain_value;
-			}
-		}
-
-		T getValue(){ return this->_value; }
-		T getDValue(){return this->_dValue;}
+	Operator<T> _operator;
+public:
+	Node() = default;
+	Node(T value): _value(value);
+	Node(Opeator<T> &&operator): _operator(operator);
 };
+
+template <typename T>
+class Operator{
+private:
+	Node<T> &_a;
+	Node<T> &_b;
+
+protected:
+	void setA(Node<int> &a){ this->_a = a; }
+	void setB(Node<int> &b){ this->_b = b; }
+
+	T getValueA(){return this->_a.getValue();}
+	T getValueB(){return this->_b.getValue();}
+
+	T Compute();
+	T Derivative();
+}
+
+template <typename T>
+class Add: public Operator{
+	Add(Node<T> &a, Node<T> &b): _a(a), _b(b) {}
+
+	T Compute(){ return this->getValueA() + this->getValueB(); }
+};
+
 
 int main(){
 	Node<int> a = 10;
 	Node<int> b = 20;
-	Node<int> c = 30;
-	Node<int> e(a, b, c);
 
-	e.compute();
-
-	e.derivative(1);
-
-	std::cout << a.getDValue() << "\n";
-	std::cout << b.getDValue() << "\n";
+	Node<int> c(Add(a, b));
 
 	return 0;
 }
