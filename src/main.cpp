139f9b703fa5b492e6a9c648c65fa5330a3f064e
Sokmontrey <sythatsokmontrey@gmail.com>
2023-01-04 16:33:36 -0500

    tried some different design to create Operator

--- src/main.cpp
+++ src/main.cpp
@@ -1,107 +1,112 @@
 #include <iostream>
 #include <vector>
-
-template <template <typename> class OP, typename T>
-class Operator;
+#include <cmath>
 
 template <typename T>
-class Node;
-
-template <typename T>
-class Node {
+class Node{
 	private:
 		T _value;
 		T _d_value;
+
 	public:
 		Node() = default;
 		Node(T value): _value(value) {}
 
-		T GetValue(){ return this->_value; }
-		T GetDValue(){ return this->_d_value; }
+		T getValue(){ return this->_value; }
+		T getDValue(){ return this->_d_value; }
 
-		void SetDValue(T d_value){ this->_d_value = d_value; }
+		void setDValue(T d_value){ this->_d_value = d_value; }
 };
 
+/*
 template <template <typename> class OP, typename T>
-class Operator {
+class Node{
 	private:
-		Node<T> &_a = temp;
-		Node<T> &_b;
-		
-		bool _is_only_a = false;
+		Node<T> *_a = nullptr;
+		Node<T> *_b = nullptr;
+
 	public:
-		Operator() = default;
-		Operator(Node<T> &a): _a(a) {
-			this->_is_only_a = true;
-		}
-		Operator(Node<T> &a, Node<T> &b): _a(a), _b(b) {}
+		Node() = default;
+		Node(Node<T> *a): _a(a) { }
+		Node(Node<T> *a, Node<T> *b): _a(a), _b(b) { }
 
-		T Compute() {
-			if(this->_is_only_a)
-				return OP<T>::Compute( this->_a.GetValue() );
+		T compute(){
+			if(_b)
+				return OP<T>::compute( _a->getValue(), _b->getValue() );
 			else
-				return OP<T>::Compute( this->_a.GetValue(), this->_b.GetValue() );
+				return OP<T>::compute( _a->getValue() );
 		}
 
-		T ComputeD(bool is_a=true, bool is_update_node=true){
+		T computeD(bool is_a=true, bool is_update_node=true){
+			//TODO: manage if the operator is single variable
 			T d_value;
-			if(this->_is_only_a){
-				d_value = OP<T>::ComputeD( 
-						this->_a.GetValue(), 
-						is_a );
-			}else{
-				d_value = OP<T>::ComputeD( 
-						this->_a.GetValue(), 
-						this->_b.GetValue(), 
-						is_a );
-			}
 
-			if( is_update_node ){
-				if(is_a) this->_a.SetDValue(d_value);
-				else this->_b.SetDValue(d_value);
+			if(_b)
+				d_value = OP<T>::computeD( _a->getValue(), _b->getValue(), is_a );
+			else
+				d_value = OP<T>::computeD( _a->getValue(), is_a );
+
+			if(is_update_node){
+				is_a ? _a->setDValue(d_value) : _b->setDValue(d_value);
 			}
 
 			return d_value;
 		}
 };
+*/
 
 template <typename T>
 struct Add{
-	static T Compute(T a, T b){
+	static T compute(T a, T b=T()){
 		return a + b;
 	}
-	static T ComputeD(T a, T b, bool is_a = true){
+
+	static T computeD(T a, T b=T(), bool is_a=true){
 		return 1;
 	}
 };
 
 template <typename T>
-struct Mul{
-	static T Compute(T a, T b){
-		return a * b;
+struct ReLU{
+	static T compute(T a){
+		return a > 0 ? a : 0;
 	}
-	static T ComputeD(T a, T b, bool isA = true){
-		return isA ? b : a;
+	static T computeD(T a, bool is_a=true){
+		return a > 0 ? 1 : 0;
 	}
 };
 
-//TODO: Pow function with custom parameter
 template <typename T>
 struct Pow{
-	static T Compute(T a){
-		return a * a;
-	};
-	static T ComputeD(T a){
-		return 2 * a;
+	static T compute(T a, T b=T()){
+		return std::pow(a, b);
+	}
+	static T computeD(T a, T b=T(), bool is_a=true){
+		return b * std::pow(a, b-1);
 	}
 };
 
+//TODO:new design:
+//	Node
+//		Var
+//		Op
+//		Const
 int main(){
-	Node<int> a = 10;
-	Node<int> b = 20;
-
-	Operator<Pow, int> c(a);
-	std::cout << c.Compute() << "\n";
-
+	
 	return 0;
 }
+
+// Emery is good
+// It is free
+
+/* Dear Viewers,
+Thank you for joining me on my YouTube LIVE Stream. I hope you have enjoyed the content and found it informative.
+
+I would like to take this opportunity to say goodbye and express my gratitude for your support. It has been a pleasure interacting with you and sharing my knowledge and thoughts with you.
+
+I hope you will continue to tune in for future streams and continue to support me in my endeavors.
+
+Thank you again for joining me. I look forward to seeing you in the next stream.
+
+Sincerely,
+Sokmontrey Sythat */
