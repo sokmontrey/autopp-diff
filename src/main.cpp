ac4c9d3d2927afa36a58ff0a42c5594a047af571
Sokmontrey <sythatsokmontrey@gmail.com>
2023-01-14 15:08:36 -0500

    update DataType

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,22 @@ class DataType{
 		T _value;
 	public:
 		DataType(T value=T()): _value(value) {}
+		DataType(DataType&& other)noexcept:DataType<double>(std::move(other._value)){ }
 
 		virtual T getValue() const { return this->_value; }
-		virtual void setValue(T new_value) { this->_value = new_value; }
-		
-        virtual DataType<T> operator+(const DataType<T> &other) const { 
-			return DataType<T>(_value + other.getValue()); 
+		virtual T operator()() const { return this->_value; }
+
+		virtual void setValue(T new_value) { 
+			this->_value = new_value;
 		}
-        virtual DataType<T> operator-(const DataType<T> &other) const { 
-			return DataType<T>(_value - other.getValue()); 
+		virtual void operator=(T new_value) {
+			this->_value = new_value; 
 		}
-        virtual DataType<T> operator*(const DataType<T> &other) const { 
-			return DataType<T>(_value * other.getValue()); 
+		virtual void operator=(const DataType& other) { 
+			this->_value = other.getValue(); 
 		}
-        virtual DataType<T> operator/(const DataType<T> &other) const { 
-			return DataType<T>(_value / other.getValue()); 
+		virtual void operator=(DataType&& other) {
+			this->_value = std::move(other._value);
 		}
 };
 
@@ -37,8 +38,17 @@ class Double: public DataType<double>{
 	public:
 		Double(double value): DataType<double>(value){ }
 
-		Double operator%(const Double &other) const {
-			return Double(_value - other._value * std::trunc(_value/other._value));
+		Double operator+(const DataType& other) const {
+			return Double(this->_value + other.getValue());
+		}
+		Double operator-(const DataType& other) const {
+			return Double(this->_value - other.getValue());
+		}
+		Double operator*(const DataType& other) const {
+			return Double(this->_value * other.getValue());
+		}
+		Double operator/(const DataType& other) const {
+			return Double(this->_value / other.getValue());
 		}
 };
 
@@ -47,9 +57,52 @@ class Int: public DataType<int>{
 		Int(int value): DataType<int>(value){ }
 };
 
+/*
+ *
+ * row x col Column-major
+ *
+ *		col
+ * row | a b |
+ *	   | c d |
+ */
+template <typename T, std::size_t ROWS, std::size_t COLS>
+class Matrix: public DataType<T>{
+	private:
+		T _data[ROWS * COLS];
+
+		std::size_t _index(std::size_t row, std::size_t col){
+			return col * ROWS + row;
+		}
+
+	public:
+		Matrix() = default;
+		Matrix(T initial_value){
+			for(int row=0; row<ROWS; row++){
+				for(int col=0; col<COLS; col++){
+					this->_data[_index(row, col)] = initial_value;
+				}
+			}
+		}
+
+		T& getValue(std::size_t row, std::size_t col) const {
+			return this->_data[_index(row, col)];
+		}
+		std::size_t getRowSize() const {
+			return ROWS;
+		}
+		std::size_t getColSize() const {
+			return COLS;
+		}
+
+		T& operator()(std::size_t row, std::size_t col) {
+			return this->_data[_index(row, col)];
+		}
+};
+
 int main(){
-	Double a(10);
-	Double b(20);
+	Matrix<double, 8, 3> a(200);
+
+	std::cout << a(7, 2) << "\n";
 
 	return 0;
 }
