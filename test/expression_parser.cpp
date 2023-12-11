de14d0cea5831a73f0ca9ba6ccfea407682fb191
Sokmontrey <sythatsokmontrey@gmail.com>
2023-12-10 22:55:11 -0500

    remove old graph

--- test/expression_parser.cpp
+++ test/expression_parser.cpp
@@ -5,8 +5,12 @@ using namespace nodeflow;
 int main() {
   // std::cout << exParser("sin(cos((abc - #BCC) * $c + 1.5)) / -2*2") <<
   // std::endl;
-  Graph f("sin(cos((#ew_mul(a_bc, def) + #BCC) * $c + 0. - 1.5)) / -2*2^5");
+  // Graph f("sin(cos((#ew_mul(a_bc, def) + #BCC) * $c + 0. - 1.5)) / -2*2^5");
   // Graph f("1 / (c + -a) / (e+f) * sin(b)^2");
+  // Graph::forward(f);
+  // Graph::backward(f);
+  // Graph::clear(f);
+  // auto varNodes = Graph::getVariableNodes(f);
 
   return 0;
 }
